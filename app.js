654aec3691f32b27cc7dad9d7ca928369499ace3
Jenny Lawrence <jlawrence010@gmail.com>
2018-04-06 14:28:58 -0700

    starting to work on problem 3

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-  var multipliedNumber = a * b;
-  return [multipliedNumber, 'The product of ' + a + ' and ' + b + ' is ' + multipliedNumber];
-}
+// function multiply(a, b) { //eslint-disable-line
+//   var multipliedNumber = a * b;
+//   return [multipliedNumber, 'The product of ' + a + ' and ' + b + ' is ' + multipliedNumber];
+// }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,8 +50,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  function sum(a, b, c) { 
+      var sumThreeNumber = a + b + c;
+  } 
+  function multiply (a, b, c) {
+      var mulThreeNumber = a * b * c;
+  } 
+  return 
 }
+
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
